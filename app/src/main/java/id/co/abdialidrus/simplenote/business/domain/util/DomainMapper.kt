98aaf53f7d5e199b8a6305c92a89393e2eb7ab30
abdialidrus <muhammadabdi25@gmail.com>
2021-10-20 11:03:12 +0700

    create DomainMapper

---
+++ app/src/main/java/id/co/abdialidrus/simplenote/business/domain/util/DomainMapper.kt
@@ -0,0 +1,9 @@
+package id.co.abdialidrus.simplenote.business.domain.util
+
+interface DomainMapper <T, DomainModel>{
+
+    fun mapToDomainModel(model: T): DomainModel
+
+    fun mapFromDomainModel(domainModel: DomainModel): T
+
+}
