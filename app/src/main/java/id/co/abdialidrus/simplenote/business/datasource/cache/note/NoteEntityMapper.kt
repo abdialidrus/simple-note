4d9c8ddd7fda9291675fde9d65b3676ae674faa2
abdialidrus <muhammadabdi25@gmail.com>
2021-10-20 11:12:39 +0700

    create Note entity mapper

---
+++ app/src/main/java/id/co/abdialidrus/simplenote/business/datasource/cache/note/NoteEntityMapper.kt
@@ -0,0 +1,36 @@
+package id.co.abdialidrus.simplenote.business.datasource.cache.note
+
+import id.co.abdialidrus.simplenote.business.domain.Note
+import id.co.abdialidrus.simplenote.business.domain.util.DomainMapper
+
+class NoteEntityMapper : DomainMapper<NoteEntity, Note> {
+    override fun mapToDomainModel(model: NoteEntity): Note {
+        return Note(
+            id = model.id,
+            title = model.title,
+            body = model.body,
+            categoryId = model.categoryId,
+            createdAt = model.createdAt,
+            updatedAt = model.updatedAt
+        )
+    }
+
+    override fun mapFromDomainModel(domainModel: Note): NoteEntity {
+        return NoteEntity(
+            id = domainModel.id,
+            title = domainModel.title,
+            body = domainModel.body,
+            categoryId = domainModel.categoryId,
+            createdAt = domainModel.createdAt,
+            updatedAt = domainModel.updatedAt
+        )
+    }
+
+    fun fromEntityList(initial: List<NoteEntity>): List<Note> {
+        return initial.map { mapToDomainModel(it) }
+    }
+
+    fun toEntityList(initial: List<Note>): List<NoteEntity> {
+        return initial.map { mapFromDomainModel(it) }
+    }
+}
