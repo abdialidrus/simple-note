0cf058528c3160c6340c8b76503e924b3970ec5f
abdialidrus <muhammadabdi25@gmail.com>
2021-10-20 11:38:57 +0700

    update get note interactor

--- app/src/main/java/id/co/abdialidrus/simplenote/business/interactor/note/GetNotes.kt
+++ app/src/main/java/id/co/abdialidrus/simplenote/business/interactor/note/GetNotes.kt
@@ -1,13 +1,54 @@
 package id.co.abdialidrus.simplenote.business.interactor.note
 
 import id.co.abdialidrus.simplenote.business.datasource.cache.note.NoteDao
+import id.co.abdialidrus.simplenote.business.datasource.cache.note.NoteEntityMapper
+import id.co.abdialidrus.simplenote.business.domain.model.Note
+import id.co.abdialidrus.simplenote.business.domain.util.DataState
+import id.co.abdialidrus.simplenote.business.domain.util.MessageType
+import id.co.abdialidrus.simplenote.business.domain.util.Response
+import id.co.abdialidrus.simplenote.business.domain.util.UIComponentType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class GetNotes(
-    private val noteDao: NoteDao
+    private val noteDao: NoteDao,
+    private val entityMapper: NoteEntityMapper
 ) {
 
-    fun execute(){
+    fun execute(): Flow<DataState<List<Note>>> = flow {
+        emit(DataState.loading<List<Note>>())
 
+        try {
+            val cachedNotes = noteDao.getAllNotes()
+            if (cachedNotes.isEmpty()) {
+                emit(
+                    DataState.error<List<Note>>(
+                        response = Response(
+                            message = "there are no saved notes",
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        )
+                    )
+                )
+            } else {
+                emit(
+                    DataState.data(
+                        response = null,
+                        data = entityMapper.fromEntityList(cachedNotes)
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            emit(
+                DataState.error<List<Note>>(
+                    response = Response(
+                        message = e.message,
+                        uiComponentType = UIComponentType.None(),
+                        messageType = MessageType.Error()
+                    )
+                )
+            )
+        }
     }
 
 }
